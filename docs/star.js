416e97441a5cedc9d4d7ceedf4704675fbc5dbe7
DamianSwanAAJHS2 <DamianSwanAAJHS2@users.noreply.github.com>
2024-05-07 23:15:58 +0000

    Prettified Code!

--- docs/star.js
+++ docs/star.js
@@ -1,14 +1,14 @@
-const wordElement = document.getElementById('random-word');
+const wordElement = document.getElementById("random-word");
 
 async function fetchRandomWord() {
   try {
-    const response = await fetch('https://random-word-api.herokuapp.com/word');
+    const response = await fetch("https://random-word-api.herokuapp.com/word");
     const data = await response.json();
 
     document.title = data; // Set the page title to the random word
   } catch (error) {
-    console.error('Error fetching word:', error);
-    wordElement.textContent = 'Error fetching word.';
+    console.error("Error fetching word:", error);
+    wordElement.textContent = "Error fetching word.";
   }
 }
 
