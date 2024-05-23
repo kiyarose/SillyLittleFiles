3af2f755bc94bc9ce9d6cfaf2fa6c8a2b993c179
Damian Swan AAHS <damian.swan36@altoonastudent.com>
2024-05-23 09:37:29 -0400

    Set udate to full line
    Signed-off-by: Damian Swan AAHS <damian.swan36@altoonastudent.com>

--- docs/star.js
+++ docs/star.js
@@ -14,8 +14,7 @@ async function fetchRandomWord() {
 // Call the function to fetch a random word on page load
 fetchRandomWord();
 
-document.getElementById("udate").innerHTML =
-  document.getElementById("udate").innerHTML + document.lastModified;
+document.getElementById("udate").innerHTML = document.getElementById("udate").innerHTML + document.lastModified;
 // Fetch last modified date and apply it to the udate element that is on some
 // pages.
 
