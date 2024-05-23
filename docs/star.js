dd7e0dd437c6d8cef85b1f1ebaaa5574312a8ced
Restyled.io <commits@restyled.io>
2024-05-23 13:37:36 +0000

    Restyled by clang-format

--- docs/star.js
+++ docs/star.js
@@ -14,7 +14,8 @@ async function fetchRandomWord() {
 // Call the function to fetch a random word on page load
 fetchRandomWord();
 
-document.getElementById("udate").innerHTML = document.getElementById("udate").innerHTML + document.lastModified;
+document.getElementById("udate").innerHTML =
+    document.getElementById("udate").innerHTML + document.lastModified;
 // Fetch last modified date and apply it to the udate element that is on some
 // pages.
 
@@ -22,11 +23,13 @@ document.getElementById("udate").innerHTML = document.getElementById("udate").in
 function copyToClipboard(text) {
   var textarea = document.createElement("textarea");
   textarea.textContent = text;
-  textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
+  textarea.style.position =
+      "fixed"; // Prevent scrolling to bottom of page in MS Edge.
   document.body.appendChild(textarea);
   textarea.select();
   try {
-    return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+    return document.execCommand(
+        "copy"); // Security exception may be thrown by some browsers.
   } catch (ex) {
     console.warn("Copy to clipboard failed.", ex);
     return false;
