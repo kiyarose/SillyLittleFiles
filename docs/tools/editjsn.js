ade4bba0ce9cea914d1ce8d48e1f6057d7397326
Restyled.io <commits@restyled.io>
2024-09-05 14:50:38 +0000

    Restyled by prettier

--- docs/tools/editjsn.js
+++ docs/tools/editjsn.js
@@ -1,31 +1,31 @@
 // Make the JSON pretty
 function beautifyJSON() {
-  const jsonText = document.getElementById('jsonInput').value
+  const jsonText = document.getElementById("jsonInput").value;
   try {
-    const jsonData = JSON.parse(jsonText)
-    const formattedJson = JSON.stringify(jsonData, null, 2)
-    document.getElementById('output').textContent = formattedJson
+    const jsonData = JSON.parse(jsonText);
+    const formattedJson = JSON.stringify(jsonData, null, 2);
+    document.getElementById("output").textContent = formattedJson;
   } catch (error) {
-    document.getElementById('output').textContent = 'Error: Invalid JSON'
+    document.getElementById("output").textContent = "Error: Invalid JSON";
   }
 }
 // Make sure the JSON isnt crap
 function validateJSON() {
-  const jsonText = document.getElementById('jsonInput').value
+  const jsonText = document.getElementById("jsonInput").value;
   try {
-    JSON.parse(jsonText)
-    document.getElementById('output').textContent = 'Valid JSON'
+    JSON.parse(jsonText);
+    document.getElementById("output").textContent = "Valid JSON";
   } catch (error) {
-    document.getElementById('output').textContent = 'Error: Invalid JSON'
+    document.getElementById("output").textContent = "Error: Invalid JSON";
   }
 }
 // Download the good JSON
 function downloadJSON() {
-  const jsonText = document.getElementById('jsonInput').value
-  const filename = 'out.json'
-  const blob = new Blob([ jsonText ], {type : 'application/json'})
-  const link = document.createElement('a')
-  link.href = URL.createObjectURL(blob)
-  link.download = filename
-  link.click()
+  const jsonText = document.getElementById("jsonInput").value;
+  const filename = "out.json";
+  const blob = new Blob([jsonText], { type: "application/json" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
 }
