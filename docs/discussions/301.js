b8f6ef62cdfd68eda45bc01d01840a882f60c787
Restyled.io <commits@restyled.io>
2024-09-21 21:22:51 +0000

    Restyled by clang-format

--- docs/discussions/301.js
+++ docs/discussions/301.js
@@ -3,22 +3,23 @@ s1.style.display = 'none'
 console.log('Set visibility to none to prevent dual clicking')
 
 // Function to toggle the visibility of the dropdown
-function DropdownSet () {
+function DropdownSet() {
   const s1 = document.getElementById('script1')
   s1.style.display = 'none'
   console.log('Set visibility to none to prevent dual clicking')
 }
 // Toggle Dropdown
-function toggleDropdown (dropdownId) {
+function toggleDropdown(dropdownId) {
   const dropdown = document.getElementById(dropdownId)
   if (dropdown.style.display === 'none') {
     dropdown.style.display = 'block'
-  } else {
+  }
+  else {
     dropdown.style.display = 'none'
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   fetchcreds() // Call fetchcreds on page load
 })
 window.dataLayer = window.dataLayer || []
